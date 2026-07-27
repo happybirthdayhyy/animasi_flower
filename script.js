0c550a31c015ebd3c99e42e5464a30488101f838
anam <freeakunbot@gmail.com>
2026-07-27 21:20:18 +0700

    Implement special envelope opening animation
    Add click event for special envelope animation.

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ onload = () => {
   }, 1000);
 };
 
+// === KODE UNTUK ANIMASI AMPLOP SPESIAL ===
+const specialEnvelope = document.getElementById('envelope-wrapper');
+let isSpecialEnvelopeOpened = false;
+
+if (specialEnvelope) {
+    specialEnvelope.addEventListener('click', () => {
+        if (!isSpecialEnvelopeOpened) {
+            specialEnvelope.classList.add('is-open');
+            isSpecialEnvelopeOpened = true; 
+        }
+    });
+}
